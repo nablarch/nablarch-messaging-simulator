915e2afd9a9a5da8aa5936264642e2eb5c2b3dfa
hijiri <3445928+hiji@users.noreply.github.com>
2023-12-11 14:44:49 +0900

    IBM MQとの通信にnablarch-wmq-adaptorを使用するように変更（再マージ） (#7)
    * feat: MQとの通信にnablarch-wmq-adaptorを利用するよう変更
    
    * feat: 依存ライブラリ（WebSphere MQ）に関する記載を削除
    
    * docs: WebSphere MQ→IBM MQに文言変更
    
    * feat: 実行モジュールの作成処理修正
    
    * feat: 実行モジュールの作成処理修正
    
    * feat: jarの名称からバージョンを除外
    
    * feat: defaultTimeToLiveを変数化
    
    * doc: MQのバージョンの記載を除去
    
    * feat: 不要になったビルド関連ファイルを削除
    
    * feat: compiler バージョンを設定
    
    ---------
    
    Co-authored-by: oguchi-shiori03 <oguchi.shiori@tis.co.jp>

--- src/main/java/please/change/me/messaging/wmq/provider/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * WebSphere MQを使用したメッセージング機能の実装クラスを配置するパッケージ。
- * 
- * @since 1.1
- */
-package please.change.me.messaging.wmq.provider;
--- src/main/mom-outgoing-startup.bat
+++ src/main/mom-outgoing-startup.bat
@@ -1,5 +1,5 @@
 @echo off
 
-java -Drequests-to-send="mom-RequestsToSend-103.csv" -cp lib\\*;src\\main\\resources;messaging-simu.jar nablarch.fw.launcher.Main -diConfig outgoing-simulator-component-configuration.xml -requestPath xxx -userId xxx
+java -Drequests-to-send="mom-RequestsToSend-103.csv" -cp lib\\*;src\\main\\resources;nablarch-messaging-simulator.jar nablarch.fw.launcher.Main -diConfig outgoing-simulator-component-configuration.xml -requestPath xxx -userId xxx
 
 pause
